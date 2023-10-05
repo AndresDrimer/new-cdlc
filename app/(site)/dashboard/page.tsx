9c522843f422910ec549b621be56d7a215a50030
AndresDrimer <andresdrimer@hotmail.com>
2023-10-04 22:29:36 -0300

    first commit

---
+++ app/(site)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { signOut } from "next-auth/react"
+
+function Dashboard() {
+  return (
+    <div><h1>DASHBOARD</h1>
+    
+    <button onClick={()=>signOut()}>Log Out</button>
+    </div>
+  )
+}
+
+export default Dashboard
